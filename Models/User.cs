2af372eadc32ac0c027b99450e95a1e82ac17129
Pierre <ezotose@gmail.com>
2021-04-05 15:56:10 +0100

    [ADD] User Model

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+namespace BackEnd_RESTProject.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Skillset { get; set; }
+        public bool Avaible { get; set; }
+    }
+}
