d934909c7dd2f64249ace1bec3e68653459df94a
julien calisto <julien.calisto@epita.fr>
2021-04-20 17:48:53 +0100

    remove forgotpass

--- Services/UserServices.cs
+++ Services/UserServices.cs
@@ -17,18 +17,18 @@ namespace BackEnd_RESTProject.Services
         User Create(User user, string password);
         void Update(User user, string currentPassword, string password, string confirmPassword, bool Avaible, string Skillset);
         void Delete(int id);
-        string ForgotPassword(string username);
+        //string ForgotPassword(string username);
     }
 
     public class UserService : IUserService
     {
         private Context _context;
-        private readonly IEmailService _emailService;
+        //private readonly IEmailService _emailService;
 
-        public UserService(Context context, IEmailService emailService)
+        public UserService(Context context)
         {
             _context = context;
-            _emailService = emailService;
+           //_emailService = emailService;
         }
 
         public User Authenticate(string username, string password)
@@ -167,10 +167,9 @@ namespace BackEnd_RESTProject.Services
             }
             return hashstring;
         }
-
+        /*
         public string ForgotPassword(string username)
         {
-            throw new NotImplementedException();
             if (string.IsNullOrEmpty(username))
             {
                 throw new AppException("Username does not exist");
@@ -199,7 +198,7 @@ namespace BackEnd_RESTProject.Services
                 return new string("your account does not exist");
             }
         }
-
+        /*
         private static string GenerateKey(int keyLength)
         {
             RNGCryptoServiceProvider rngCryptoServiceProvider = new RNGCryptoServiceProvider();
@@ -212,5 +211,6 @@ namespace BackEnd_RESTProject.Services
             }
             return hashstring;
         }
+        */
     }
 }
